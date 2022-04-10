378ee1051d8b10dfb788caf2a31f4f340e24a6ec
Jannatul Ferdous <jannatpinky92@gmail.com>
2022-04-10 02:44:51 -0400

    login and sign in component created

---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '.Login.css';
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <h1>this is Login</h1>
+        </div>
+    );
+};
+
+export default Login;
